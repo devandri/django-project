1c612763963c2437ca6b135a764338da1d202b86
Andri 33 <saputroandri1991@gmail.com>
2025-07-08 13:10:42 +0700

    Add permission on bookings

--- reservation_app/views.py
+++ reservation_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def home(request):
@@ -15,3 +16,4 @@ class MenuViewSet(viewsets.ModelViewSet):
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
