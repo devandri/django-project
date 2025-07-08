484b5bed6b7d4146ee456408658c017ce03dd22d
Andri 33 <saputroandri1991@gmail.com>
2025-07-08 11:50:43 +0700

    setup mysql database and urls

--- reservation_app/models.py
+++ reservation_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+class Booking(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    guests = models.IntegerField()
